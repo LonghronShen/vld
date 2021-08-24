93b1bc1e8632995e52aae90aa04fd2f8732bbb34
Steven Hangger <longhronshen@hotmail.com>
2021-08-24 09:41:58 +0800

    update

--- src/ntapi.cpp
+++ src/ntapi.cpp
@@ -22,19 +22,45 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
+
+#ifndef VLDBUILD
 #define VLDBUILD
+#endif
+
 #include "ntapi.h"
 
 // Global function pointers for explicit dynamic linking with NT APIs that can't
 // be load-time linked (there is no import library available for these).
-LdrLoadDll_t        LdrLoadDll;
-LdrLoadDllWin8_t    LdrLoadDllWin8;
-RtlAllocateHeap_t   RtlAllocateHeap;
-RtlFreeHeap_t       RtlFreeHeap;
+LdrLoadDll_t LdrLoadDll;
+LdrLoadDllWin8_t LdrLoadDllWin8;
+RtlAllocateHeap_t RtlAllocateHeap;
+RtlFreeHeap_t RtlFreeHeap;
 RtlReAllocateHeap_t RtlReAllocateHeap;
 
-LdrGetDllHandle_t   LdrGetDllHandle;
+LdrGetDllHandle_t LdrGetDllHandle;
 LdrGetProcedureAddress_t LdrGetProcedureAddress;
-LdrUnloadDll_t      LdrUnloadDll;
+LdrUnloadDll_t LdrUnloadDll;
 LdrLockLoaderLock_t LdrLockLoaderLock;
 LdrUnlockLoaderLock_t LdrUnlockLoaderLock;
+
+#if _WIN32_WINNT < 0x0600
+USHORT WINAPI RtlCaptureStackBackTrace(__in ULONG FramesToSkip, __in ULONG FramesToCapture, __out PVOID * BackTrace,
+                                       __out_opt PULONG BackTraceHash)
+                                       {
+   RtlCaptureStackBackTrace_t func =
+    (RtlCaptureStackBackTrace_t)(GetProcAddress(LoadLibrary(L"kernel32.dll"), "RtlCaptureStackBackTrace"));
+
+   if(func == NULL)
+       return; // WOE 29.SEP.2010
+
+   // Quote from Microsoft Documentation:
+   // ## Windows Server 2003 and Windows XP:  
+   // ## The sum of the FramesToSkip and FramesToCapture parameters must be less than 63.
+   const int kMaxCallers = 62; 
+
+   void* callers[kMaxCallers];
+   int count = (func)(0, kMaxCallers, callers, NULL);
+   for(i = 0; i < count; i++)
+      printf(TraceFile, "*** %d called from %016I64LX\n", i, callers[i]);
+                                       }
+#endif
